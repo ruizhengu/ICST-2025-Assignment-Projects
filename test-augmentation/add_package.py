0cef01ad45df1ad72f3234f675c81eb32801b3f0
Ruizhen Gu <ravenna1031@gmail.com>
2023-10-03 20:36:57 +0100

    add_package - read from command line

--- test-augmentation/add_package.py
+++ test-augmentation/add_package.py
@@ -1,11 +1,15 @@
 import os
+import sys
 
 # TEST_PATH = "/Users/ruizhengu/Projects/APR-as-AAT/com1003_cafe_8/src/test/java/uk/ac/sheffield/com1003/cafe/randoop"
-TEST_PATH = "/Users/ruizhengu/Experiments/APR-as-AAT/anonymised-submissions/1/src/test/java/uk/ac/sheffield/com1003/cafe/randoop"
+TEST_PATH = sys.argv[1]
 
-for file in os.listdir(TEST_PATH):
-    if file.endswith(".java"):
-        with open(os.path.join(TEST_PATH, file), "r+") as f:
-            content = f.read()
-            f.seek(0, 0)
-            f.write("package uk.ac.sheffield.com1003.cafe.randoop;" + "\n\n" + content)
+if not os.path.exists(os.path.dirname(TEST_PATH)):
+    print("Please enter a valid path")
+else:
+    for file in os.listdir(TEST_PATH):
+        if file.endswith(".java"):
+            with open(os.path.join(TEST_PATH, file), "r+") as f:
+                content = f.read()
+                f.seek(0, 0)
+                f.write("package uk.ac.sheffield.com1003.cafe.randoop;" + "\n\n" + content)
