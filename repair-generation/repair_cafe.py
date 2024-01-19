4f703677c34b9c197d3fc76ba3ecbcb68f474ae2
Ruizhen Gu <ravenna1031@gmail.com>
2024-01-19 15:59:01 +0000

    need to run on hpc

--- repair-generation/repair_cafe.py
+++ repair-generation/repair_cafe.py
@@ -1,3 +1,5 @@
+import json
+import os
 import re
 import shutil
 import subprocess
@@ -14,11 +16,17 @@ class RepairCafe:
         self.ref_path = Path("/Users/ruizhengu/Projects/APR-as-AAT/com1003_cafe/com1003_cafe_8")
         self._main_path = Path("src/main/java/uk/ac/sheffield/com1003/cafe")
         self._test_path = Path("src/test/java/uk/ac/sheffield/com1003/cafe")
+        self._astor_output = utils.ASTOR_OUTPUT
+        self.patch_results = Path(
+            "/Users/ruizhengu/Projects/APR-as-AAT/repair-generation/results/results_cafe.xlsx")
+        utils.create_excel(self.patch_results)
 
     def replace_tests(self, submission):
         destination = submission / "src/test/java/uk/ac/sheffield/com1003/cafe"
         utils.empty_directory(destination)
-
+        destination = destination / "junit4"
+        if not destination.exists():
+            destination.mkdir(parents=True)
         for item in self.test_suite.iterdir():
             if item.is_dir():
                 shutil.copytree(item, destination / item.name)
@@ -48,6 +56,14 @@ class RepairCafe:
 
                 with open(build_gradle, "w") as f:
                     f.writelines(lines)
+                with build_gradle.open("r") as f:
+                    content = f.readlines()
+                with build_gradle.open("w") as f:
+                    for line in content:
+                        if "dependencies {" in line:
+                            line += "testImplementation 'org.apache.commons:commons-lang3:3.0'\n"
+                            line += "testImplementation 'com.github.javaparser:javaparser-core-serialization:3.25.1'\n"
+                        f.write(line)
 
                 # change the environmental variable JAVA_HOME to 1.8
                 cmd = f"gradle build -x test -p {submission}"
@@ -98,11 +114,63 @@ class RepairCafe:
                 utils.replace_package(target_name, "uk.ac.sheffield.com1003.cafe",
                                       "uk.ac.sheffield.com1003.cafe.solution")
 
+    def pre_processing(self):
+        ref_classes, ref_tests = self.get_ref()
+        submissions_main, submissions_test, roots = self.get_submission()
+        self.copy_ref(ref_classes, submissions_main)
+        return roots
+
+    def repair(self, root, root_index):
+        utils.run_cmd(f"gradle build -p {root}")
+
+        astor_command = f"java -cp /Users/ruizhengu/Experiments/APR-as-AAT/astor/target/astor-*-jar-with-dependencies.jar fr.inria.main.evolution.AstorMain -mode jgenprog -srcjavafolder /src/main/java/ -srctestfolder /src/test/java/ -binjavafolder /build/classes/java/main/ -bintestfolder /build/classes/java/test/ -location {root} -scope global -out {self._astor_output}"
+        utils.run_cmd(astor_command)
+        astor_output = self._astor_output / f"AstorMain-{root.split('/')[-1]}"
+        astor_output_json = astor_output / "astor_output.json"
+        with astor_output_json.open("r") as file:
+            patches = json.load(file)["patches"]
+        if patches:
+            print(f"Patch found - {root}")
+            data = {
+                # "ID": patch_count,
+                "Project": "Cafe",
+                "Patch": f"AstorMain-Cafe-{root_index}",
+                "Submission": str(root)
+            }
+            new_path = self.rename_output(astor_output)
+            utils.append_excel(self.patch_results, data)
+            utils.update_patch_paths(new_path, new_path.name)
+            return new_path
+        else:
+            print(f"No patch found - {root}")
+            shutil.rmtree(astor_output)
+            return None
+
+    def rename_output(self, astor_output):
+        pattern = rf"AstorMain-Cafe-1-(\d+)"
+        max_digit = 0
+        for file in self._astor_output.iterdir():
+            match = re.match(pattern, file.name)
+            if match:
+                digit = int(match.group(1))
+                max_digit = max(max_digit, digit)
+        if max_digit > 0:
+            new_digit = max_digit + 1
+        else:
+            new_digit = 1
+        new_name = self._astor_output / f"AstorMain-Cafe-1-{new_digit}"
+
+        if astor_output.exists():
+            os.rename(astor_output, new_name)
+            return new_name
+        return None
+
 
 if __name__ == '__main__':
     submissions = "/Users/ruizhengu/Experiments/APR-as-AAT/anonymised-submissions"
     repair_cafe = RepairCafe(submissions)
-    ref_classes, ref_tests = repair_cafe.get_ref()
-    submissions_main, submissions_test, roots = repair_cafe.get_submission()
-    repair_cafe.copy_ref(ref_classes, submissions_main)
+    roots = repair_cafe.pre_processing()
     repair_cafe.build_version()
+    for root_index, root in enumerate(roots, start=1):
+        print(f"Processing submission {root_index}/{len(roots)}")
+        repair_cafe.repair(root, root_index)
