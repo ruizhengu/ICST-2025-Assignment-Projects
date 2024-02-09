481f02a434052fc54880c78665c75d25ab655d92
Ruizhen Gu <ravenna1031@gmail.com>
2024-02-09 12:01:26 +0000

    update incremental repair - use gradle to check the number of negative tests

--- repair-generation/mutation/partial_repair_mutant.py
+++ repair-generation/mutation/partial_repair_mutant.py
@@ -32,39 +32,61 @@ class PartialRepairMutant:
         for index, mutant in enumerate(self.mutant_roots, start=1):
             mutant_name = mutant.split('/')[-1]
             positive_tests, negative_tests, num_tests = utils.gradle_get_tests(mutant)
-            initial_positive_tests = len(positive_tests)
-            # If the mutant is fixed with a test, keep the test in the list, otherwise drop it
-            incremental_test = list(positive_tests)
+            if len(negative_tests) == 0:
+                print("=" * 10 + f" No Negative Tests > {mutant_name} " + "=" * 10)
+                continue
             if len(negative_tests) > 0:
+                print("=" * 10 + f" Partial Repair Start > {mutant_name} " + "=" * 10)
                 print(f"Number of Negative Tests: {len(negative_tests)}")
                 self.empty_backup()
+                # Put all negative tests in the backup folders
                 for test in negative_tests:
                     self.test_backup(mutant, test)
                 for test_index, test in enumerate(negative_tests, start=1):
-                    incremental_test.append(test)
+                    # Put negative tests in the proje
                     self.move_test(mutant, test)
                     patch = self.repair_mutant.repair(mutant)
-                    if patch == "No suspicious line":
+                    if patch == f"No suspicious line > Mutant {mutant_name} > Test {test}":
                         print("No suspicious line")
                         continue
                     elif patch is not None:
                         utils.apply_patch(patch / "astor_output.json")
-                        self.save_patch_results(mutant_name, patch, incremental_test, num_tests)
                     else:
-                        incremental_test.pop()
-                        self.delete_test(mutant, test)
-            # Check if partial repair works
-            if len(incremental_test) == num_tests:
-                print(f"Partial Repair - Fixed > Mutant {mutant}")
-            elif len(incremental_test) > initial_positive_tests:
-                print(f"Partial Repair - Improved > Mutant {mutant}")
+                        # Drop the failed test, put it to the backup,
+                        # Checking number of negative tests after partial repair
+                        self.test_backup(mutant, test)
+                    _, negative_tests_intermediate, _ = utils.gradle_get_tests(mutant)
+                    self.save_patch_results(mutant_name, patch, len(negative_tests_intermediate), num_tests)
+            # Get the number of negative tests to check if partial repair works
+            self.restore_backup(mutant)
+            _, negative_tests_repaired, _ = utils.gradle_get_tests(mutant)
+            if len(negative_tests_repaired) == 0:
+                print(f"Partial Repair - Fixed > Mutant {mutant_name}")
+            elif len(negative_tests_repaired) < len(negative_tests):
+                print(f"Partial Repair - Improved > Mutant {mutant_name}")
+            else:
+                print(f"Partial Repair - Failed > Mutant {mutant}")
 
-    def save_patch_results(self, mutant_name, patch, incremental_test, num_tests):
+    def restore_backup(self, mutant):
+        backup_src = self.home_repair / self._backup_src
+        backup_bin = self.home_repair / self._backup_bin
+        for src in backup_src.iterdir():
+            if src.name.endswith(".java"):
+                source_src = Path(mutant) / self._src_test / src
+                shutil.move(backup_src, source_src)
+        for _bin in backup_bin.iterdir():
+            if _bin.name.endswith(".class"):
+                source_bin = Path(mutant) / self._bin_test / _bin
+                shutil.move(backup_bin, source_bin)
+
+    def save_patch_results(self, mutant_name, patch, negative_tests_repaired, num_tests):
+        if patch is None:
+            patch = "None"
         data = {
             "Project": "Cafe",
             "Mutant": mutant_name,
             "Patch": str(patch).split('/')[-1],
-            "Passed Tests": f"{len(incremental_test)} / {num_tests}"
+            "Passed Tests": f"{negative_tests_repaired} / {num_tests}"
         }
         utils.append_excel(self.patch_results, data)
 
