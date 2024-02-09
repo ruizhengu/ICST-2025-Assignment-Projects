51744f33c96d2315e30e65915905f7ee6f4f0ee4
Ruizhen Gu <ravenna1031@gmail.com>
2024-02-09 15:32:13 +0000

    add resource - model mutants

---
+++ resource/model_mutants/cafe_2/src/test/java/uk/ac/sheffield/com1003/cafe/TestPrintPendingOrdersMultiple.java
@@ -0,0 +1,110 @@
+package uk.ac.sheffield.com1003.cafe;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.sheffield.com1003.cafe.Cafe;
+import uk.ac.sheffield.com1003.cafe.Recipe;
+import uk.ac.sheffield.com1003.cafe.exceptions.TooManyIngredientsException;
+import uk.ac.sheffield.com1003.cafe.ingredients.Coffee;
+import uk.ac.sheffield.com1003.cafe.ingredients.Water;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class TestPrintPendingOrdersMultiple {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    protected Recipe createEspressoRecipe() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Coffee());
+            espresso.addIngredient(new Water());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+    protected Recipe createEspressoRecipeAlt() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Water());
+            espresso.addIngredient(new Coffee());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+    protected Recipe createEspressoRecipeIncomplete() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Water());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+//    protected ArrayList<String> getOutLines() {
+//        Stream<String> lines = outContent.toString().lines();
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        lines.forEach(arrayList::add);
+//        return arrayList;
+//    }
+
+    protected void resetOutLines() {
+        outContent.reset();
+    }
+
+//    protected ArrayList<String> getErrLines() {
+//        Stream<String> lines = errContent.toString().lines();
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        lines.forEach(arrayList::add);
+//        return arrayList;
+//    }
+
+    @Test(timeout = 5000)
+    public void printPendingOrdersMultiple() throws Exception {
+        Cafe cafe = new Cafe("Central Perk", 2, 2);
+        cafe.addRecipe(createEspressoRecipe());
+        cafe.placeOrder("Espresso", "Jose", 3);
+        cafe.placeOrder("Espresso", "Mari-Cruz", 5);
+        cafe.printPendingOrders();
+//        ArrayList<String> lines = getOutLines();
+//        assertEquals(3, lines.size());
+//        assertEquals("Pending Orders:", lines.get(0));
+//        assertEquals("Order: Espresso; For: Jose; Paid: 3.0", lines.get(1));
+//        assertEquals("Order: Espresso; For: Mari-Cruz; Paid: 5.0", lines.get(2));
+    }
+
+
+
+
+}
---
+++ resource/model_mutants/cafe_4/src/test/java/uk/ac/sheffield/com1003/cafe/TestSyrupToString.java
@@ -0,0 +1,114 @@
+package uk.ac.sheffield.com1003.cafe;
+
+import org.apache.commons.lang3.reflect.ConstructorUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.sheffield.com1003.cafe.Recipe;
+import uk.ac.sheffield.com1003.cafe.exceptions.TooManyIngredientsException;
+import uk.ac.sheffield.com1003.cafe.ingredients.Coffee;
+import uk.ac.sheffield.com1003.cafe.ingredients.Unit;
+import uk.ac.sheffield.com1003.cafe.ingredients.Water;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+import static uk.ac.sheffield.com1003.cafe.ingredients.Unit.ML;
+
+public class TestSyrupToString  {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    protected Recipe createEspressoRecipe() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Coffee());
+            espresso.addIngredient(new Water());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+    protected Recipe createEspressoRecipeAlt() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Water());
+            espresso.addIngredient(new Coffee());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+    protected Recipe createEspressoRecipeIncomplete() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Water());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+//    protected ArrayList<String> getOutLines() {
+//        Stream<String> lines = outContent.toString().lines();
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        lines.forEach(arrayList::add);
+//        return arrayList;
+//    }
+
+    protected void resetOutLines() {
+        outContent.reset();
+    }
+
+//    protected ArrayList<String> getErrLines() {
+//        Stream<String> lines = errContent.toString().lines();
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        lines.forEach(arrayList::add);
+//        return arrayList;
+//    }
+
+    @Test(timeout = 5000)
+    public void testSyrupToString() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException {
+        Class<?> syrupClass = Class.forName("uk.ac.sheffield.com1003.cafe.ingredients.Syrup");
+        Object o = ConstructorUtils.invokeConstructor(syrupClass, "caramel");
+        assertTrue(syrupClass.isInstance(o));
+        int amount = (int)FieldUtils.readField(o, "amount", true);
+        Unit unit = (Unit)FieldUtils.readField(o, "unit", true);
+        String s = (String)MethodUtils.invokeMethod(o, "toString");
+        assertEquals("Syrup [unit=" + unit + ", amount=" + amount + ", flavour=caramel]", s);
+    }
+
+
+
+
+
+
+
+
+}
