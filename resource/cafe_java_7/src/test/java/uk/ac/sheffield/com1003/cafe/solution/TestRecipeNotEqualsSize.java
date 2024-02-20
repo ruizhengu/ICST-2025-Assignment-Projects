098c060248335e18c4580bafb726023dea9ce65f
Ruizhen Gu <ravenna1031@gmail.com>
2024-02-20 17:07:49 +0000

    add model solution package in cafe java 7

---
+++ resource/cafe_java_7/src/test/java/uk/ac/sheffield/com1003/cafe/solution/TestRecipeNotEqualsSize.java
@@ -0,0 +1,108 @@
+package uk.ac.sheffield.com1003.cafe.solution;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.sheffield.com1003.cafe.solution.exceptions.TooManyIngredientsException;
+import uk.ac.sheffield.com1003.cafe.solution.ingredients.Coffee;
+import uk.ac.sheffield.com1003.cafe.solution.ingredients.Water;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class TestRecipeNotEqualsSize  {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    protected Recipe createEspressoRecipe() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Coffee());
+            espresso.addIngredient(new Water());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+    protected Recipe createEspressoRecipeAlt() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Water());
+            espresso.addIngredient(new Coffee());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+    protected Recipe createEspressoRecipeIncomplete() {
+        try {
+            Recipe espresso = new Recipe("Espresso", 1.5, Recipe.Size.SMALL, 2);
+            espresso.addIngredient(new Water());
+            return espresso;
+        } catch (TooManyIngredientsException exc) {
+            System.err.println(exc.getMessage());
+            return null;
+        }
+    }
+
+//    protected ArrayList<String> getOutLines() {
+//        Stream<String> lines = outContent.toString().lines();
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        lines.forEach(arrayList::add);
+//        return arrayList;
+//    }
+
+    protected void resetOutLines() {
+        outContent.reset();
+    }
+
+//    protected ArrayList<String> getErrLines() {
+//        Stream<String> lines = errContent.toString().lines();
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        lines.forEach(arrayList::add);
+//        return arrayList;
+//    }
+
+    @Test(timeout = 5000)
+    public void testRecipeNotEqualsSize() {
+        Recipe nothing = new Recipe("Nothing", 1, Recipe.Size.SMALL, 0);
+        Recipe dummy = new Recipe("Nothing", 1, Recipe.Size.LARGE, 0);
+        assertNotEquals(nothing, dummy);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
