ae1187c04fbd233446dcf80d6f3db1329fef637c
Ruizhen Gu <ravenna1031@gmail.com>
2024-03-01 16:33:18 +0000

    add ARJA - supoort Java 8

---
+++ arja/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation(fileTree("libs") { include("*.jar") })
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("org.eclipse.platform:org.eclipse.core.runtime:3.13.0")
+        force("org.eclipse.core:org.eclipse.core.resources:3.9.1.v20140825-1431")
+
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ arja/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "arja-gradle"
+
