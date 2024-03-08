45e4e48905f8e328a364094b6badb43e4b8268a8
Ruizhen Gu <ravenna1031@gmail.com>
2024-03-08 10:55:08 +0000

    complete method coverage

--- repair-generation/hpc_cafe.sh
+++ repair-generation/hpc_cafe.sh
@@ -7,7 +7,7 @@ module load Java/8.362
 module Anaconda3/2022.05
 module load Python/3.8.6-GCCcore-10.2.0
 #conda init bash
-conda activate APR-as-AAT
+#conda activate APR-as-AAT
 
 #export PYTHONPATH="${PYTHONPATH}:/mnt/parscratch/users/acp22rg/APR-as-AAT/APR4Grade/repair-generation"
 
