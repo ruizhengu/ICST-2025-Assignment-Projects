aaa774d4de030dcc18f227016b939a52438c3941
Ruizhen Gu <ravenna1031@gmail.com>
2024-03-10 14:21:23 +0000

    replace method content with model solution

--- repair-generation/cafe/partial_repair_cafe_arja.py
+++ repair-generation/cafe/partial_repair_cafe_arja.py
@@ -9,19 +9,32 @@ class PartialRepairCafeArja:
         self.model_solution = Path("/Users/ruizhengu/Projects/APR-as-AAT/resource/cafe_java_8")
         self.method_file_json = Path("/Users/ruizhengu/Projects/APR-as-AAT/repair-generation/lib/method_files.json")
 
-    def replace_method(self, file_path, method_name, content):
-        with open(file_path, 'r') as file:
+    def replace_method(self, submission, method_name, content):
+        method_file = Path(self.get_method_path(method_name))
+        file_path = submission / method_file
+        with open(file_path, "r") as file:
             code = file.read()
-
         pattern = re.compile(
-            r'(public|protected|private|static|\s) +[\w<>\[\]]+\s+' + re.escape(method_name) + r'\([^\)]*\) \{[^\}]*\}',
+            rf'(public|protected|private|static|\s) +[\w<>\[\]]+\s+{re.escape(method_name.split(".")[1])}\s*\([^\)]*\)\s*(throws\s+[\w,\s]+)?\s*\{{',
             re.DOTALL)
-
-        new_method = f"public void {method_name}() {{{content}}}"
-        updated_code = re.sub(pattern, new_method, code)
-
-        with open(file_path, 'w') as file:
-            file.write(updated_code)
+        match = pattern.search(code)
+        if not match:
+            print(f"Method {method_name} not found.")
+            return
+        start_index = match.start()
+        end_index = match.end() - 1
+        brace_count = 1
+        i = end_index + 1
+        while i < len(code) and brace_count > 0:
+            if code[i] == '{':
+                brace_count += 1
+            elif code[i] == '}':
+                brace_count -= 1
+            i += 1
+        new_code = code[:start_index] + code[start_index:end_index] + content + code[i:]
+        with open(file_path, "w") as file:
+            file.write(new_code)
+        print(f"Method {method_name} in {file_path} replaced successfully.")
 
     def get_model_method_content(self, method_name):
         method_file = Path(self.get_method_path(method_name))
@@ -42,7 +55,6 @@ class PartialRepairCafeArja:
                 elif code[i] == '}':
                     brace_count -= 1
                 i += 1
-            # method_content = code[start_index:i].strip()
             return code[start_index:i].strip()
         else:
             print(f"Method {method_name} not found in {file_path}")
@@ -64,6 +76,9 @@ class PartialRepairCafeArja:
 
 
 if __name__ == '__main__':
-    method = "Cafe.printMenu"
+    method = "App.main"
+    submission = Path("/Users/ruizhengu/Experiments/APR-as-AAT/cafe_java_8")
+
     p = PartialRepairCafeArja()
-    print(p.get_model_method_content(method))
+    content = p.get_model_method_content(method)
+    p.replace_method(submission, method, content)
