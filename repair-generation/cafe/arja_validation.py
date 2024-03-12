d455944f49cde163ce8bcf4dd85bf571e5130716
Ruizhen Gu <ravenna1031@gmail.com>
2024-03-12 18:03:59 +0000

    count number of failed tests

---
+++ repair-generation/cafe/arja_validation.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+class ArjaValidation:
+    def __init__(self):
+        self.submission_path = Path("/Users/ruizhengu/Experiments/APR-as-AAT/anonymised-submissions")
+
+    def get_num_negative_tests(self):
+        pass
