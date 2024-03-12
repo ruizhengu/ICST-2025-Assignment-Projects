e963acd5b83ee14a636addbb054274bf311e8008
Ruizhen Gu <ravenna1031@gmail.com>
2024-03-12 14:40:10 +0000

    update arja repairing script

--- repair-generation/cafe/repair_cafe_arja.py
+++ repair-generation/cafe/repair_cafe_arja.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import shutil
 from pathlib import Path
@@ -7,13 +8,22 @@ import utils
 
 class RepairIntroClass:
     def __init__(self):
-        self.home_path = Path("/mnt/parscratch/users/acp22rg/APR-as-AAT/IntermediateJava")
+        self.home_path = Path("/mnt/parscratch/users/acp22rg/APR/IntermediateJava")
         self.submission_list = [submission for submission in self.home_path.iterdir() if
                                 submission.is_dir() and submission.name != ".git"]
-        self.model_solution = Path("/mnt/parscratch/users/acp22rg/APR-as-AAT/APR4Grade/resource/cafe_java_8")
+        self.model_solution = Path("/mnt/parscratch/users/acp22rg/APR/APR4Grade/resource/cafe_java_8")
         self.model_test_suite = self.model_solution / "src/test/java/uk/ac/sheffield/com1003/cafe"
         self._main_path = Path("src/main/java/uk/ac/sheffield/com1003/cafe")
         self._test_path = Path("src/test/java/uk/ac/sheffield/com1003/cafe")
+        self.arja_output = self.logging_init()
+
+    def logging_init(self):
+        arja_output = Path("/mnt/parscratch/users/acp22rg/APR/cafe_arja_default")
+        if not arja_output.exists():
+            os.mkdir(arja_output)
+        arja_log = arja_output / "arja.log"
+        logging.basicConfig(filename=arja_log, level=logging.INFO)
+        return arja_output
 
     def replace_build_gradle(self, submission):
         build_gradle_source = self.model_solution / "build.gradle"
@@ -74,35 +84,39 @@ class RepairIntroClass:
             print("*" * 5 + f" {submission} compilation finish " + "*" * 5)
 
     def arja(self):
-        arja_path = "/mnt/parscratch/users/acp22rg/APR-as-AAT/APR4Grade/arja"
-        arja_build_cmd = f"{arja_path}/gradlew run -p {arja_path} build"
-        utils.run_cmd(arja_build_cmd)
+        arja_path = "/mnt/parscratch/users/acp22rg/APR/arja"
+        # arja_build_cmd = f"{arja_path}/gradlew run -p {arja_path} build"
+        # utils.run_cmd(arja_build_cmd)
         for submission in self.submission_list:
             path_src = submission / "src"
             path_bin_src = submission / "build/classes/java/main"
             path_bin_test = submission / "build/classes/java/test"
-            path_dependency = Path("/mnt/parscratch/users/acp22rg/APR-as-AAT/APR4Grade/dependency")
+            path_dependency = Path("/mnt/parscratch/users/acp22rg/APR/APR4Grade/dependency")
             dependencies = [str(file) for file in path_dependency.glob('**/*.jar') if file.name != ".DS_Store"]
             dependencies = ":".join(dependencies)
-            path_output = Path("/mnt/parscratch/users/acp22rg/APR-as-AAT/cafe_arja_default")
-            if not path_output.exists():
-                os.mkdir(path_output)
-            path_output = path_output / submission.name
-            if not path_output.exists():
-                os.mkdir(path_output)
-            # arja_cmd = f"cd {arja_path} && java -cp \"lib/*:bin\" us.msu.cse.repair.Main ArjaE -DsrcJavaDir {path_src} -DbinJavaDir {path_bin_src} -DbinTestDir {path_bin_test} -Ddependences {dependencies} -DpatchOutputRoot {path_output}"
-            arja_cmd = f"{arja_path}/gradlew run -p {arja_path} --args='ArjaE -DsrcJavaDir {path_src} -DbinJavaDir {path_bin_src} -DbinTestDir {path_bin_test} -Ddependences {dependencies} -DpatchOutputRoot {path_output}'"
+            # path_output = Path("/mnt/parscratch/users/acp22rg/APR/cafe_arja_default")
+            # if not path_output.exists():
+            #     os.mkdir(path_output)
+            arja_output = self.arja_output / submission.name
+            if not arja_output.exists():
+                os.mkdir(arja_output)
+            arja_cmd = f"cd {arja_path} && java -cp \"lib/*:bin\" us.msu.cse.repair.Main ArjaE -DsrcJavaDir {path_src} -DbinJavaDir {path_bin_src} -DbinTestDir {path_bin_test} -Ddependences {dependencies} -DpatchOutputRoot {arja_output}"
+            # arja_cmd = f"{arja_path}/gradlew run -p {arja_path} --args='ArjaE -DsrcJavaDir {path_src} -DbinJavaDir {path_bin_src} -DbinTestDir {path_bin_test} -Ddependences {dependencies} -DpatchOutputRoot {path_output}'"
             print("=" * 10 + f" ARJA -> {submission} " + "=" * 10)
             # print(arja_cmd)
             arja_output = utils.run_cmd(arja_cmd)
             # print(arja_output)
+            self.logging(submission)
+
+    def logging(self, submission):
+        arja_output = self.arja_output / submission.name
+        if not any(arja_output.iterdir()):
+            logging.info(f"Repair {submission} completed > No patch generated.")
+        else:
+            logging.info(f"Repair {submission} completed > Patches generated.")
 
 
 if __name__ == '__main__':
-    # repair = RepairIntroClass()
-    # repair.compile_submissions()
-    # repair.arja()
-    path_dependency = Path("/Users/ruizhengu/Projects/APR-as-AAT/dependency")
-    dependencies = [str(file) for file in path_dependency.glob('**/*.jar') if file.name != ".DS_Store"]
-    dependencies = ":".join(dependencies)
-    print(dependencies)
+# repair = RepairIntroClass()
+# repair.compile_submissions()
+# repair.arja()
