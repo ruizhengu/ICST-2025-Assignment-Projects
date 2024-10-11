389ec122c257b62eb24b91571a78d971689f8b79
Ruizhen Gu <ravenna1031@gmail.com>
2024-10-11 17:41:14 +0100

    generate prompts

--- nero/repair/llm.py
+++ nero/repair/llm.py
@@ -38,6 +38,55 @@ class LLMRepair:
             signature = match.group().strip()
         return class_content, signature
 
+    def get_model_method(self, solution, method_path):
+        file_path = self.model_solution / method_path
+        method_name = solution.name.split(".")[1]
+
+        with open(file_path, "r") as file:
+            code = file.read()
+        pattern = re.compile(
+            rf'(public|protected|private|static|\s) +[\w<>\[\]]+\s+{re.escape(method_name)}\s*\([^\)]*\)\s*(throws\s+[\w,\s]+)?\s*\{{',
+            re.DOTALL)
+        match = pattern.search(code)
+        if match:
+            start_index = match.start()
+            brace_count = 1
+            i = match.end()
+            while i < len(code) and brace_count > 0:
+                if code[i] == '{':
+                    brace_count += 1
+                elif code[i] == '}':
+                    brace_count -= 1
+                i += 1
+            return code[start_index:i].strip()
+        else:
+            return None
+
+    def get_test_content(self, test):
+        test_class = test.split(".")[-2]
+        test_method = test.split(".")[-1]
+        test_path = self.model_solution / "src/test/java/uk/ac/sheffield/com1003/cafe" / f"{test_class}.java"
+        with open(test_path, "r") as file:
+            code = file.read()
+        pattern = re.compile(
+            rf'(public|protected|private|static|\s) +[\w<>\[\]]+\s+{re.escape(test_method)}\s*\([^\)]*\)\s*(throws\s+[\w,\s]+)?\s*\{{',
+            re.DOTALL)
+        match = pattern.search(code)
+        if match:
+            start_index = match.start()
+            brace_count = 1
+            i = match.end()
+            while i < len(code) and brace_count > 0:
+                if code[i] == '{':
+                    brace_count += 1
+                elif code[i] == '}':
+                    brace_count -= 1
+                i += 1
+            return code[start_index:i].strip()
+        else:
+            return None
+
+
     def get_failing_tests(self, intermediate):
         failing_tests_error_message = {}
         with open(self.method_coverage_json) as f:
@@ -45,7 +94,11 @@ class LLMRepair:
         failing_tests = data[intermediate.parent.name][intermediate.name]["tests"]
         for test in failing_tests:
             error_message = self.get_error_message(intermediate, test)
-            failing_tests_error_message[test] = error_message
+            test_content = self.get_test_content(test)
+            failing_tests_error_message[test] = {
+                "test": test_content,
+                "error": error_message
+            }
         return failing_tests_error_message
 
     def get_error_message(self, intermediate, test):
@@ -61,15 +114,28 @@ class LLMRepair:
 
         return failed_message
 
-
-    def get_model_solutions(self):
-        pass
-
     def generate_prompt(self, intermediate):
         method_path = self.get_method_path(intermediate.name)
         class_content, method_signature = self.get_class_content(intermediate, method_path)
         failing_test_error_message = self.get_failing_tests(intermediate)
-        print(intermediate.name, failing_test_error_message)
+        model_method = self.get_model_method(intermediate, method_path)
+
+        prompt = "You are an automatic program repair tool. Your task is to fix a buggy method with in its class.\n"
+        prompt += "The following class is where the buggy method within:\n"
+        prompt += f"```{class_content}```\n"
+        prompt += f"The buggy method is `{method_signature}`.\n"
+        prompt += "The method fails the following tests:\n"
+        for failing_test in failing_test_error_message:
+            prompt += f"```{failing_test_error_message[failing_test]['test']}\n# error message:\n{failing_test_error_message[failing_test]['error']}\n```"
+        prompt += "This is the reference solution of the buggy method, do not directly use this as the answer.\n"
+        prompt += f"```{model_method}```\n"
+        prompt += "Please provide a fixed version of the buggy method, do not use the reference solution, make the minimal edits to the original buggy method\n"
+        prompt += "Return only the fixed buggy method, within a code block."
+
+        prompt_txt = intermediate / "prompt.txt"
+        with open(prompt_txt, "w") as f:
+            f.write(prompt)
+        return prompt_txt
 
     def get_method_path(self, method_name):
         with open(self.method_file_json) as f:
@@ -80,15 +146,13 @@ class LLMRepair:
         intermediate_submission = self.dataset / submission
         for intermediate in intermediate_submission.iterdir():
             if intermediate.is_dir():
-                self.generate_prompt(intermediate)
-                # method_path = self.get_method_path(intermediate.name)
-                # self.generate_prompt(submission, method_path)
+                prompt_txt = self.generate_prompt(intermediate)
 
     def count_repairs(self):
         purged_count = 0
         fixed_submissions = 0
         fixed_bugs = 0
-        purged_path = Path("/Users/ruizhengu/Experiments/intermediates")
+        purged_path = Path("/Users/ruizhengu/Experiments/patches_1")
         purged_submissions = (_ for _ in purged_path.iterdir() if _.is_dir())
         for submission in purged_submissions:
             fully_patched = True
@@ -111,5 +175,5 @@ class LLMRepair:
 if __name__ == '__main__':
     l = LLMRepair()
     # l.repair_results()
-    # l.count_repairs()
-    l.repair("1")
+    l.count_repairs()
+    # l.repair("1")
